74469f9073b8f3170813b11aaa5bf39cacf396ea
Toru Tamura <torut@ttsoft.jp>
2013-03-06 11:05:10 +0900

    modified .emacs.el for add ruby-insert-magic-comment-if-needed.

--- .emacs.el
+++ .emacs.el
@@ -127,7 +127,7 @@
 
 (defun magit-setup-diff ()
   ;; 'allではなくtにすると現在選択中のhunkのみ強調表示する
-  (setq magit-diff-refine-hunk 't)
+  (setq magit-diff-refine-hunk t)
   ;; diff用のfaceを設定する
   (diff-mode-setup-faces)
   ;; diffの表示設定が上書きされてしまうのでハイライトを無効にする
@@ -187,8 +187,33 @@
 (add-hook 'ruby-mode-hook
           '(lambda ()
              (inf-ruby-keys)))
-(custom-set-variables
- '(ruby-insert-encoding-magic-comment nil))
+;; (custom-set-variables
+;;  '(ruby-insert-encoding-magic-comment nil))
+(defun ruby-insert-magic-comment-if-needed ()
+  "バッファのcoding-systemをもとにmagic commentをつける。"
+  (when (and (eq major-mode 'ruby-mode)
+             (find-multibyte-characters (point-min) (point-max) 1))
+    (save-excursion
+      (goto-char 1)
+      (when (looking-at "^#!") 
+        (forward-line 1))
+      (if (re-search-forward "^#.+coding" (point-at-eol) t)
+          (delete-region (point-at-bol) (point-at-eol))
+        (open-line 1))
+      (let* ((coding-system (symbol-name buffer-file-coding-system))
+             (encoding (cond ((string-match "japanese-iso-8bit\\|euc-j" coding-system)
+                              "euc-jp")
+                             ((string-match "shift.jis\\|sjis\\|cp932" coding-system)
+                              "shift_jis")
+                             ((string-match "utf-8" coding-system)
+                              "utf-8"))))
+        (insert (format "# -*- coding: %s -*-" encoding))
+        )
+      )
+    )
+  )
+(add-hook 'before-save-hook 'ruby-insert-magic-comment-if-needed)
+
 
 ;; Rails
 ;; (setq load-path (cons "~/.lisp/emacs-rails" load-path))
