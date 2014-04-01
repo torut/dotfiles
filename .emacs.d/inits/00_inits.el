3bc38216da96f0c4b794227eb2ef43d75f50b2b2
Toru Tamura <torut@ttsoft.jp>
2014-04-01 18:09:17 +0900

    modified emacs 00_init.

--- .emacs.d/inits/00_inits.el
+++ .emacs.d/inits/00_inits.el
@@ -74,6 +74,13 @@
 
 ;; ファイルリストで ls を呼ぶオプションを指定
 (setq dired-listing-switches "-alh")
+;; diredを2つのウィンドウで開いている時に、デフォルトの移動orコピー先をもう一方のdiredで開いているディレクトリにする
+(setq dired-dwim-target t)
+;; ディレクトリを再帰的にコピーする
+(setq dired-recursive-copies 'always)
+;; diredバッファでC-sした時にファイル名だけにマッチするように
+(setq dired-isearch-filenames t)
+
 
 ;; ファイルリストでリネームができるようにする
 (require 'wdired)
