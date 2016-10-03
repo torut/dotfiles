d33fa001873a45f37e3bcd0ee8fbbc06a8a3cd7c
Toru TAMURA <torut@ttsoft.jp>
2016-10-03 18:36:53 +0900

    [modify] js2-mode using whitespace to indent.

--- .emacs.d/inits/20_mode-js.el
+++ .emacs.d/inits/20_mode-js.el
@@ -2,16 +2,23 @@
 ;; (autoload 'javascript-mode "javascript" nil t)
 ;; (add-to-list 'auto-mode-alist (cons "\\.\\(js\\|as\\|json\\|jsn\\)\\'" 'javascript-mode))
 (autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist (cons "\\.\\(js\\|as\\|json\\|jsn\\)\\'" 'js2-mode))
+(add-to-list 'auto-mode-alist (cons "\\.\\(js\\|json\\)\\'" 'js2-mode))
+(add-hook 'js2-mode-hook
+		  '(lambda ()
+			 (setq js2-basic-offset 2
+				   indent-tabs-mode nil ; t => tab, nil => space
+			 )
+		  )
+)
 
 ;; coffeeモードの設定
-(autoload 'coffee-mode "coffee-mode" "Major mode for editing CoffeeScript." t)
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-(defun coffee-custom ()
-  "coffee-mode-hook"
-  (and (set (make-local-variable 'tab-width) 2)
-       (set (make-local-variable 'coffee-tab-width) 2))
-  )
-(add-hook 'coffee-mode-hook '(lambda() (coffee-custom)))
+;; (autoload 'coffee-mode "coffee-mode" "Major mode for editing CoffeeScript." t)
+;; (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+;; (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+;; (defun coffee-custom ()
+;;   "coffee-mode-hook"
+;;   (and (set (make-local-variable 'tab-width) 2)
+;;        (set (make-local-variable 'coffee-tab-width) 2))
+;;   )
+;; (add-hook 'coffee-mode-hook '(lambda() (coffee-custom)))
 
