522950b58c949764f801aefd0fe427de46528288
Toru TAMURA <torut@ttsoft.jp>
2026-08-04 13:22:27 +0900

    feat: modern構成でtool-bar-modeをGUI起動時のみ有効化
    コメントアウトされていたtool-bar-mode -1を有効化し、
    initial-frame-alistと同じdisplay-graphic-pガード内に含める。

--- .emacs.d/inits/00_inits.el
+++ .emacs.d/inits/00_inits.el
@@ -6,8 +6,8 @@
   :init
   ;;;; 表示関係 (GUI起動時のみ適用)
   (when (display-graphic-p)
-    (setq initial-frame-alist '((top . 1) (left . 1) (width . 160) (height . 52))))
-  ;; (tool-bar-mode -1)
+    (setq initial-frame-alist '((top . 1) (left . 1) (width . 160) (height . 52)))
+    (tool-bar-mode -1))
   (menu-bar-mode -1)
   (setq scroll-conservatively 20
         scroll-margin 2
