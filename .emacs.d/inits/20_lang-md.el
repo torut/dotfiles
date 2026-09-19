1726d69a627ff97b4a82f06ba9b9026be945e1a5
Toru TAMURA <torut@ttsoft.jp>
2026-09-19 17:54:29 +0900

    fix: markdown-modeのインデントにタブを使わないよう設定

--- .emacs.d/inits/20_lang-md.el
+++ .emacs.d/inits/20_lang-md.el
@@ -1,4 +1,9 @@
 ;; markdown
 (use-package markdown-mode
   :ensure t ; markdown-mode は外部パッケージなのでインストールを指示
-  :mode ("\\.md\\'" . markdown-mode))
+  :mode ("\\.md\\'" . markdown-mode)
+  :hook
+  (markdown-mode .
+                 (lambda ()
+                   ;; インデントにタブを使わない
+                   (setq-local indent-tabs-mode nil))))
